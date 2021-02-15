66cf58ef3204c99e859bc7635fb5224056804c41
David Freifeld <freifeld.david@gmail.com>
2021-02-14 17:57:59 -0800

    Add logging library dependency

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "vec.h"
+#include "../log/src/log.h"
 
 int main() {
     Vec test = vec_new();
     VEC_PUSH(int, &test, 32)
-
     printf("test[0]=%d", *VEC_AT(int, &test, 0));
+    log_info("Finished.");
 }
