e434fe71fc382f66284a09937e4bee9585d060ca
David Freifeld <freifeld.david@gmail.com>
2021-02-15 13:26:19 -0800

    Debug hashmap insertion
    This is a bad design for the hashmap

--- src/hashmap.c
+++ src/hashmap.c
@@ -7,7 +7,7 @@
 //#endif
 
 // djb2 algorithm
-size_t hash_str(const char* str)
+static size_t hash_str(const char* str)
 {
     size_t hash = 5381;
     int c;
@@ -19,9 +19,8 @@ size_t hash_str(const char* str)
 #define hash(x) _Generic((x), int: (size_t) x,   \
                          const char*: hash_str)
 
-
 typedef struct hashmap {
-    void* keys; // For recomputing hashes properly.
+    void* keys;
     void* vals;
     size_t k_sz;
     size_t v_sz;
@@ -40,19 +39,24 @@ static void _hashmap_cons(hashmap_t* h, size_t key_sz, size_t val_sz, size_t len
 #define hashmap_cons(hmap, k_type, v_type, len) _hashmap_cons(hmap, sizeof(k_type), sizeof(v_type), len)
 #define hashmap_cons_exp(hmap, k_sz, v_sz, len) _hashmap_cons(hmap, k_sz, v_sz, len)
 
-void _hashmap_ins(hashmap_t* h, size_t index, void* key, void* val)
+static void _hashmap_ins(hashmap_t* h, size_t index, void* key, void* val)
 {
-    memcpy(h->vals+index, val, h->v_sz);
-    memcpy(h->keys+index, key, h->k_sz);
+    memcpy(((char*)h->vals)+index*4, val, h->v_sz);
+    memcpy(((char*)h->keys)+index*4, key, h->k_sz);
 }
 
-#define hashmap_ins(hmap, k, v) _hashmap_ins(hmap, hash(k), &k, v)
-   
+#define hashmap_ins(hmap, k, v) _hashmap_ins(hmap, hash(k), &k, &v)
+
+static void _hashmap_get(hashmap_t* h, size_t index)
+{
+    return 
+}
+
 int main()
 {
     hashmap_t h;
-    hashmap_cons(&h, int, const char*, 10);
-    int v = 2;
-    hashmap_ins(&h, v, "abc");
-    printf("%s\n", ((char**)h.vals)[1]);
+    hashmap_cons(&h, int, int, 10);
+    int k = 2;
+    int v = 3;
+    hashmap_ins(&h, v, k);
 }
