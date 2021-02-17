919be37c12e497e7d971dafb14e0379d8461991e
David Freifeld <freifeld.david@gmail.com>
2021-02-16 16:56:30 -0800

    Type size checking
    Committing broken code in case this needs to be used

--- src/hashmap.c
+++ src/hashmap.c
@@ -2,67 +2,85 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <log.h>
 
-//#if !(__STDC__==1 && __STDC_VERSION >= 201112L)
-//#error C11 compiler required.
-//#endif
+#define is_same_type(a, b)    __builtin_types_compatible_p(typeof(a), typeof(b))
 
 // djb2 algorithm
 static size_t hash_str(const char* str)
 {
     size_t hash = 5381;
     int c;
-    // equivalent to hash * 33 + c, which is
+    // equivalent to hash * 33 + c
     while ((c = *str++)) hash = ((hash << 5) + hash) + c;
     return hash;
 }
 
-#define hash(x) _Generic((x), int: (size_t) x,   \
-                         uint64_t: (size_t) x % UINT32_MAX,   \
-                         const char*: hash_str)
+int _exit_fatal(const char* what)
+{
+    log_fatal(what);
+    exit(1);
+    return 1;
+}
+
+#define hash(x) _Generic((x), int: (size_t) x,                          \
+                         uint64_t: (size_t) x % UINT32_MAX,             \
+                         const char*: hash_str,                         \
+                         default: _exit_fatal("Invalid type passed to hash(). Please supply your own hash function instead."))
 
 typedef struct hashmap {
+    const char* __ktype;
+    const char* __vtype;
     void* keys;
     void* vals;
+#ifdef DEBUG
     size_t k_sz;
     size_t v_sz;
+#endif
     size_t len;
 } hashmap_t;
 
-static void _hashmap_cons(hashmap_t* h, size_t key_sz, size_t val_sz, size_t len)
+static void _hashmap_cons(hashmap_t* h, const char* ktype, const char* vtype, )
 {
+    h->__ktype = ktype;
+    h->__vtype = vtype;
     h->vals = malloc(val_sz * len);
     h->keys = malloc(key_sz * len);
+#ifdef DEBUG
     h->k_sz = key_sz;
     h->v_sz = val_sz;
+#endif
     h->len = len;
 }
 
-#define hashmap_cons(hmap, k_type, v_type, len) _hashmap_cons(hmap, sizeof(k_type), sizeof(v_type), len)
-#define hashmap_cons_exp(hmap, k_sz, v_sz, len) _hashmap_cons(hmap, k_sz, v_sz, len)
+#define hashmap_cons(hmap, k_type, v_type, len) _hashmap_cons(hmap, #k_type, #v_type, sizeof(k_type), sizeof(v_type), len)
+/* #define hashmap_cons_exp(hmap, k_sz, v_sz, len) _hashmap_cons(hmap, k_sz, v_sz, len) */
 
-static void _hashmap_ins(hashmap_t* h, size_t index, void* key, void* val)
+
+#ifdef DEBUG
+#define check_size(sz, expected, ctx) if(sz != expected) log_warn("Type size mismatch in value of %s (declared size %zu != %s size %zu)", ctx, expected, ctx, sz)
+#endif
+
+#ifdef DEBUG
+#define hashmap_ins(T1, T2, hmap, k, v) check_size(sizeof(T1), hmap->k_sz, "hashmap inserted key"); \
+    check_size(sizeof(T2), hmap->v_sz, "hashmap inserted val"); \
+    ((T1*)(hmap)->keys)[hash(k) % (hmap)->len] = k; \
+    ((T2*)(hmap)->vals)[hash(k) % (hmap)->len] = v;
+#else
+#define hashmap_ins(T1, T2, hmap, k, v) ((T2*)(hmap)->vals)[hash(k) % (hmap)->len] = v; \
+    ((T1*)(hmap)->keys)[hash(k) % (hmap)->len] = k;
+#endif
+#define hashmap_ins_arr(T1, T2, hmap, k, v, k_sz, v_sz) hashmap_ins(T1, T2, hmap, malloc(k_sz), malloc(v_sz))
+
+
+static void* _hashmap_get(hashmap_t* h, size_t index, size_t v_sz)
 {
-    memcpy(h->vals+(index*h->v_sz), val, h->v_sz);
-    memcpy(h->keys+(index*h->k_sz), key, h->k_sz);
+    return h->vals + (index * v_sz);
 }
 
-#define hashmap_ins_arr(hmap, k, v) _hashmap_ins(hmap, hash(k) % hmap->len, &k, &v)
-#define hashmap_ins(T, hmap, k, v) ((T*)(hmap)->vals)[hash(k) % (hmap)->len] = v;
-
-static void* _hashmap_get(hashmap_t* h, size_t index)
-{
-    return h->vals + (index * h->v_sz);
-}
-
-#define hashmap_get(T, hmap, k) *((T*)_hashmap_get(hmap, hash(k) % (hmap)->len))
-
-int main()
-{
-    hashmap_t h;
-    hashmap_cons(&h, int, int, 10);
-    int k = 2;
-    int v = 3;
-    hashmap_ins(int, &h, k, v);
-    printf("%d\n", hashmap_get(int, &h, 2));
-}
+#ifdef DEBUG
+#define hashmap_get(T, hmap, k) check_size(sizeof(T), hmap->v_sz, "hashmap read value"); \
+        *((T*)_hashmap_get(hmap, hash(k) % (hmap)->len, sizeof(T)))
+#else
+#define hashmap_get(T, hmap, k) *((T*)_hashmap_get(hmap, hash(k) % (hmap)->len, sizeof(T)))
+#endif
