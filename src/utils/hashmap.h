30a2d666ae99f02180ec1d914f17e34fb8542cce
David Freifeld <freifeld.david@gmail.com>
2021-02-25 14:45:14 -0800

    Add hashmap function descriptions

--- src/utils/hashmap.h
+++ src/utils/hashmap.h
@@ -1,3 +1,5 @@
+#include <stddef.h>
+
 typedef struct hashmap_t {
     void* keys;
     void* vals;
@@ -7,8 +9,17 @@ typedef struct hashmap_t {
     size_t filled;
 } hashmap_t;
 
-void hashmap_init(hashmap_t* h, size_t ksize, size_t vsize);
+// Generates a new hashmap_t.
+hashmap_t hashmap_new(size_t ksize, size_t vsize);
+
+// Sets a key-value pair in the hashmap_t passed to it. Returns 0x0 for success, 0x1 for failure.
 int hashmap_set(hashmap_t* h, void* k, void* v);
+
+// Returns address of value associated with key in hashmap if it exists, otherwise returns 0x0 for no value or 0x1 for full table.
 void* hashmap_get(hashmap_t* h, void* k);
+
+// Deletes key in hashmap (but not value).
 int hashmap_del(hashmap_t* h, void* k);
+
+// Frees a hashmap.
 void hashmap_free(hashmap_t* h);
