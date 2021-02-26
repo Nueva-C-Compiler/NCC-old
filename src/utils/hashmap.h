8508399ddd6fd886607403518103256e57c67d74
David Freifeld <freifeld.david@gmail.com>
2021-02-26 08:52:53 -0800

    Fix header comments

--- src/utils/hashmap.h
+++ src/utils/hashmap.h
@@ -9,17 +9,31 @@ typedef struct hashmap_t {
     size_t filled;
 } hashmap_t;
 
-// Generates a new hashmap_t. Returns NULL if allocation failed.
+/**
+ * Constructs a new hashmap_t.
+ * Returns constructed hashmap_t, returns NULL if allocation failed.
+ */
 hashmap_t hashmap_new(size_t ksize, size_t vsize);
 
-// Sets a key-value pair in the hashmap_t passed to it. Returns false for failure, true for success.
+/**
+ * Sets a key-value pair in the hashmap_t passed to it.
+ * @retuens Returns false for failure, true for success.
+ */
 bool hashmap_set(hashmap_t* h, void* k, void* v);
 
-// Returns address of value associated with key in hashmap if it exists, otherwise returns NULL if no matching key found.
+/**
+ * Get value associated with key in hashmap.
+ * @returns Returns address of value associated with key in hashmap if it exists, otherwise returns NULL if no matching key found.
+ */
 void* hashmap_get(hashmap_t* h, void* k);
 
-// Deletes key in hashmap (but not value). Returns false for failure, true for success.
+/**
+ * Deletes key in hashmap (but not value).
+ * @returns Returns false for failure, true for success.
+ */
 bool hashmap_del(hashmap_t* h, void* k);
 
-// Frees a hashmap.
+/**
+ * Frees the hashmap.
+ */
 void hashmap_free(hashmap_t* h);
--- src/utils/panic.h
+++ src/utils/panic.h
@@ -2,4 +2,8 @@ enum panic_type {
     UNREACHABLE_STATE,
     NULLPTR,
 };
+
+/**
+ * Logs error message 'what' to FATAL, exits.
+ */
 void panic(const char* what, enum panic_type type);
